18f97433ef16dec8ab89619debf38d4e3c598dcd
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-07 11:54:56 -0700

    Qualitative section added.

--- test.php
+++ test.php
@@ -10,7 +10,6 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 	</head>
 	<body>
-		<h1>Pro Dev</h1>
 
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -18,7 +17,24 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
-		<section></section>
+		<header>
+			<div class="container">
+				<div class="jumbotron">
+					<h1 class="display-3">Pro Dev</h1>
+				</div>
+			</div>
+		</header>
+
+		<section>
+			<div class="container">
+				<h2>Qualitative</h2>
+				<ul class="list-group">
+					<li class="list-group-item">Good Organization skills</li>
+					<li class="list-group-item">Great time management skills</li>
+					<li class="list-group-item">Great people skills</li>
+				</ul>
+			</div>
+		</section>
 		<section></section>
 		<section></section>
 
